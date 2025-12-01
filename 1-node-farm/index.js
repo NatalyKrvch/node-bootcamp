28a045b5d91e349171ab2c82b0c4208e641a5d5a
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-12-01 23:27:38 +0200

    Async, non-blocking reading/writing files

---
+++ 1-node-farm/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+
+//Sync, blocking
+
+// const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8')
+// console.log(textIn)
+//
+// const textOut = `this is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('./starter/txt/output.txt', textOut)
+// console.log('Done')
+
+//Async, non-blocking
+fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log('error');
+
+    fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3);
+
+            fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}}`, 'utf-8', err => {
+                console.log('written');
+            });
+        })
+    })
+})
+
+console.log('Done')
+
+
