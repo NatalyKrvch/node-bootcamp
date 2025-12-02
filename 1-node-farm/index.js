f0761233ddaf36fb51409a2a30b65920d43f5b46
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-12-02 22:57:44 +0200

    add first server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,8 @@
 const fs = require('fs')
+const http = require('http')
+
+//////////////////////////////////////////////////////////////////////
+//FILES
 
 //Sync, blocking
 
@@ -10,21 +14,30 @@ const fs = require('fs')
 // console.log('Done')
 
 //Async, non-blocking
-fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
-    if (err) return console.log('error');
+// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
+//     if (err) return console.log('error');
+//
+//     fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
+//
+//             fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}}`, 'utf-8', err => {
+//                 console.log('written');
+//             });
+//         })
+//     })
+// })
+//
+// console.log('Done')
 
-    fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
-            console.log(data3);
 
-            fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}}`, 'utf-8', err => {
-                console.log('written');
-            });
-        })
-    })
+///////////////////////////////////////////////////////////////////////////////////
+//SERVER
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server!')
 })
 
-console.log('Done')
-
-
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Server started on port 8000')
+})
