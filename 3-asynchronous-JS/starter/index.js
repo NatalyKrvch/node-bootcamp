0ad52caf94d2afe5d647099ce9fc8fbda8e56023
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2025-12-17 22:27:19 +0200

    building promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,19 +1,36 @@
 const fs = require('fs')
 const superagent = require('superagent')
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-    console.log(`Breed: ${data}`)
-
-    superagent
-        .get(`https://dog.ceo/api/breed/${data}/images/random`)
-        .then(res => {
-            console.log(res.body.message)
-
-            fs.writeFile(`${__dirname}/dog-img.txt`, res.body.message, err => {
-                if (err) return console.log(err.message);
-                console.log('random dog image has saved to the file')
-            })
-    }).catch(err => {
-        console.log(err.message)
+const readFileProm = file => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err) reject('could not read file')
+            resolve(data)
+        })
     })
-})
+}
+
+const writeFileProm = (file, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, err => {
+            if (err) reject('could not write file')
+            resolve('success')
+        })
+    })
+}
+
+readFileProm(`${__dirname}/dog.txt`)
+    .then(data => {
+        console.log(`Breed: ${data}`)
+        return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+    })
+    .then(res => {
+        console.log(res.body.message)
+        return writeFileProm(`${__dirname}/dog-img.txt`, res.body.message)
+    })
+    .then(() => {
+        console.log('random dog image has saved to the file')
+    })
+    .catch(err => {
+        console.log(err)
+    })
